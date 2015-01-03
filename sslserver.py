fac89e1e2ff678b967bada46eb9805254ead39b4
Andreas Granig <agranig@sipwise.com>
2015-01-03 23:00:15 +0100

    Add geo detection and compass ring.

---
+++ sslserver.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import BaseHTTPServer, SimpleHTTPServer
+import ssl
+
+httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 8000), SimpleHTTPServer.SimpleHTTPRequestHandler)
+httpd.socket = ssl.wrap_socket (httpd.socket, certfile='localhost.pem', server_side=True)
+httpd.serve_forever()
