0627945aa320595ba249ddfe49078e35b386405d
Andreas Granig <agranig@sipwise.com>
2015-01-03 23:29:36 +0100

    Rename bomber to drone.

--- minimize.sh
+++ minimize.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-JS_OUT=bomber-min.js
-HTML_OUT=bomber.html
+JS_OUT=drone-min.js
+HTML_OUT=drone.html
 
 UGLIFY=$(which uglifyjs)
 if [ -z "$UGLIFY" ]; then
     echo "uglifyjs not found in PATH, install from https://github.com/mishoo/UglifyJS";
     exit 1;
 fi
-$UGLIFY -mt bomber.js > $JS_OUT
+$UGLIFY -mt drone.js > $JS_OUT
 
 cp index.html $HTML_OUT
-perl -pi -e 's/bomber\.js/bomber-min.js/g' $HTML_OUT
+perl -pi -e 's/drone\.js/drone-min.js/g' $HTML_OUT
 
